7f01650093c7ecc808d80e1f1032a41ab0667660
Carey Costa <careyxcosta@gmail.com>
2017-02-09 13:43:00 -0500

    updated 1:42pm

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208231845) do
+ActiveRecord::Schema.define(version: 20170209175807) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "restaurant_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170208231845) do
     t.datetime "updated_at",    null: false
     t.string   "cuisine"
     t.integer  "capcity"
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create(
+  name: "Kal and Mooy",
+  city: "Richmond Hill",
+  capcity: "18",
+  cuisine: "African",
+  image: "http"
+  )
+
+  Restaurant.create(
+    name: "Me-N-U",
+    city: "Queen West",
+    capcity: "24",
+    cuisine: "Asian Fusion",
+    image: "http"
+  )
+
+    Restaurant.create(
+      name: "Kal and Mooy",
+      city: "Richmond Hill",
+      capcity: "18",
+      cuisine: "African",
+      image: "http"
+      )
+
+      Restaurant.create(
+        name: "Blue Donkey",
+        city: "The Beaches",
+        capcity: "30",
+        cuisine: "Greek",
+        image: "http"
+      )
+
+      Restaurant.create(
+        name: "Feisty Jack",
+        city: "Etobicoke",
+        capcity: "33",
+        cuisine: "English",
+        image: "http"
+      )
+
+      Restaurant.create(
+        name: "Randy's Curbside",
+        city: "North York",
+        capcity: "14",
+        cuisine: "BBQ and Grill",
+        image: "http"
+      )
